24fec1dfc4d38764844f1602567bfea281eac2a6
Sebastian Cheung <sebastian_cheung@yahoo.com>
2014-12-19 23:52:14 +0000

    First commit

---
+++ tryFlaskHeroku.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask.ext.heroku import Heroku
+
+app = Flask(__name__)
+heroku = Heroku(app)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    heroku.init_app(app)
+    app.run()
